30f6009b76a6fa715d838c60d3208fabffbcf13b
John <113923030+Indubitably8@users.noreply.github.com>
2024-11-24 18:46:21 -0500

    IR Sensor
    Signa

--- IWantToJonkleEvinAsHeWhispersMyNameJonklerInMyEar/IWantToJonkleEvinAsHeWhispersMyNameJonklerInMyEar.ino
+++ IWantToJonkleEvinAsHeWhispersMyNameJonklerInMyEar/IWantToJonkleEvinAsHeWhispersMyNameJonklerInMyEar.ino
@@ -1,38 +1,18 @@
-//This works... I think
+bool running = false;
 
-// Pin definitions
-#define ENCODER_PIN_A 2  // Must be an interrupt pin
-#define ENCODER_PIN_B 3  // Can be any digital pin
-
-volatile int encoderPosition = 0;
-int lastEncoded = 0;
-
-void setup() {
-  pinMode(ENCODER_PIN_A, INPUT_PULLUP);
-  pinMode(ENCODER_PIN_B, INPUT_PULLUP);
-
-  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A), updateEncoder, CHANGE);
-
-  Serial.begin(9600);
+void setup(){
+  pinMode(0, INPUT);
+  pinMode(2, OUTPUT);
 }
 
-void loop() {
-  // Print encoder position
-  Serial.println(encoderPosition);
-  delay(100);
+void loop(){
+  if(digitalRead(0) == LOW){
+    if(!running){
+      run();
+    }
+  }
 }
 
-void updateEncoder() {
-  int MSB = digitalRead(ENCODER_PIN_A); // Most significant bit
-  int LSB = digitalRead(ENCODER_PIN_B); // Least significant bit
-
-  int encoded = (MSB << 1) | LSB; // Combine into single binary value
-  int sum = (lastEncoded << 2) | encoded;
-
-  if (sum == 0b0001 || sum == 0b0111 || sum == 0b1110 || sum == 0b1000)
-    encoderPosition++;
-  if (sum == 0b0010 || sum == 0b0100 || sum == 0b1101 || sum == 0b1011)
-    encoderPosition--;
-
-  lastEncoded = encoded; // Store current state
+void run(){
+    digitalWrite(2, HIGH);
 }
